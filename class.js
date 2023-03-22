cc621080d105d1c306c46161d2d3d2ffb4a8a6e8
mdtanvirgit <officialtanvir@hotmail.com>
2023-03-22 06:51:13 +0600

    javascript class object create successfylly

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-
+// ES6 defiend class way
 class PanDrive{
     constructor(capacity,color,price){
         this.capacity = capacity ;
@@ -9,4 +9,19 @@ class PanDrive{
 
 const twinmoss = new PanDrive('32GB','Black',500);
 
+const sundisk = new PanDrive("64GB","White");
+
 console.log(twinmoss);
+console.log(sundisk);
+
+
+// before of es6 object create method
+
+function StickyNote(color,price){
+    this.color = color;
+    this.price = price;
+}
+
+const yellow = new StickyNote("Gray",400);
+
+console.log(yellow);
