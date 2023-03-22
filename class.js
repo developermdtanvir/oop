f907e6d9e9f4036d2a1c8b3c9f461e9bd1af7e06
mdtanvirgit <officialtanvir@hotmail.com>
2023-03-22 06:38:57 +0600

    i am learinng object oriented programming

---
+++ class.js
@@ -0,0 +1,12 @@
+
+class PanDrive{
+    constructor(capacity,color,price){
+        this.capacity = capacity ;
+        this.color = color ;
+        this.price = price;
+    }
+}
+
+const twinmoss = new PanDrive('32GB','Black',500);
+
+console.log(twinmoss);
